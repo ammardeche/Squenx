5fc74b0399ebc0f8fb3a68d3c4ce49abac80129b
ammardeche <bibocrew135@gmail.com>
2023-09-01 23:03:10 +0100

    new upload

--- src/app/pages/trading-platform/trading-platform.component.ts
+++ src/app/pages/trading-platform/trading-platform.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-trading-platform',
   templateUrl: './trading-platform.component.html',
   styleUrls: ['./trading-platform.component.css']
 })
-export class TradingPlatformComponent {
+export class TradingPlatformComponent implements AfterViewInit {
+  
+  @ViewChild('swiperRef') swiperRef!: ElementRef;
 
+  SwiperConfig: SwiperOptions ={
+   slidesPerView: 1,
+   spaceBetween: 20,
+   // Responsive breakpoints
+   breakpoints: {
+     // when window width is >= 320px
+     320: {
+       slidesPerView: 1,
+       spaceBetween: 20
+     },
+     420: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+     // when window width is >= 480px
+   
+     // when window width is >= 640px
+     1140: {
+       slidesPerView: 4,
+       spaceBetween: 60
+     },
+
+
+
+     
+   }
+  }
+ 
+ 
+  ngAfterViewInit(): void {
+    Object.assign(this.swiperRef.nativeElement , this.SwiperConfig);
+    this.swiperRef.nativeElement.initialize();
+  }
 }
